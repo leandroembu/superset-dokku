18a25c131230840cca2a043840ed40e7f7ab786a
Leandro Ramos <leandroramos@disroot.org>
2023-10-10 15:29:31 -0300

    Implements a new login view to emdedding URLs

--- superset_config.py
+++ superset_config.py
@@ -35,5 +35,40 @@ SQLLAB_ASYNC_TIME_LIMIT_SEC = 300
 SQLLAB_TIMEOUT = 300
 SUPERSET_WEBSERVER_TIMEOUT = 300
 
-# Gamma role grants readonly access to some superset features
-PUBLIC_ROLE_LIKE = "Gamma"
+# Custom configs for embedding superset
+from flask_appbuilder.security.views import expose
+from superset.security import SupersetSecurityManager
+from flask_appbuilder.security.manager import BaseSecurityManager
+from flask_appbuilder.security.manager import AUTH_REMOTE_USER
+from flask import  redirect, request, flash
+from flask_login import login_user
+
+# Create a custom view to authenticate the user
+AuthRemoteUserView=BaseSecurityManager.authremoteuserview
+class CustomAuthUserView(AuthRemoteUserView):
+    @expose('/login/')
+    def login(self):
+        token = request.args.get('token')
+        next = request.args.get('next')
+        sm = self.appbuilder.sm
+        session = sm.get_session
+        user = session.query(sm.user_model).filter_by(username='admin').first()
+        if token == os.environ["SUPERSET_EMBED_TOKEN"]:
+            login_user(user, remember=False, force=True)
+            if (next is not None):
+                return redirect(next)
+            else:
+                return redirect(self.appbuilder.get_url_for_index)
+        else:
+            flash('Unable to auto login', 'warning')
+            return super(CustomAuthUserView,self).login()
+
+# Create a custom Security manager that overrides the CustomAuthUserView
+class CustomSecurityManager(SupersetSecurityManager):
+    authremoteuserview = CustomAuthUserView
+
+# Use our custom authenticator
+CUSTOM_SECURITY_MANAGER = CustomSecurityManager
+
+# User remote authentication
+AUTH_TYPE = AUTH_REMOTE_USER
